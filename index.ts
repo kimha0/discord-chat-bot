4c5ef1f8be7ec95b21e05c30aede00a07df235f0
kimha0 <hyoreuno@naver.com>
2020-06-23 17:04:30 +0900

    feat: add express server

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 import Discord from 'discord.js';
 import { onMessageHandle } from './message';
-
+import express from 'express';
 
 async function main() {
-  const options = {
-  };
-
   const client = new Discord.Client();
+  const app = express();
+  const port = process.env.PORT || 3000;
 
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user?.tag ?? ''}!`);
-  });
-  
+  client.on('ready', () => console.log(`Logged in as ${client.user?.tag ?? ''}!`));
   client.on('message', onMessageHandle);
-
   client.login(process.env.DISCORD_API_TOKEN);
+
+  app.get('/', (req, res) => res.send('dnf chat bot'));
+  
+  app.listen(port, () => console.log(`express running at ${port}`));
 };
 
 
